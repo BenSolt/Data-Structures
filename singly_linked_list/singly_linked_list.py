06ea9d444cb56bed932a8c221352e974ff08a105
benjamin-solt <bensolt@gmail.com>
2020-05-12 15:29:53 -0400

    working on singly linked list still

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -16,20 +16,32 @@ class LinkedList:
         while (temp): 
             print(temp.data)
             temp = temp.next
-  
+
+
   
 if __name__=='__main__': 
-  
+
     
     llist = LinkedList() 
   
     llist.head = Node('list 1') 
     second = Node('list 2') 
-    third = Node('list 3') 
+    third = Node('list 3')
+    fourth = Node('list 4')
   
     llist.head.next = second 
     second.next = third
+    third.next = fourth
   
     llist.printList() 
     
+    while True:
+        cmd = input("Enter a number:").split(' ')
+
+        if cmd[0] == "a":
+            print("list 1")
+        elif cmd[0] == 'b':     
+            print("list 2")
+        
+   
     
